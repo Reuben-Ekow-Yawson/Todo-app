dc991405138da500b7baa7a164efbf1530732ce6
Reuben Yawson <reuben.yawson@amalitech.com>
2022-06-04 19:37:50 +0000

    Built todo app with React TypeScript

---
+++ src/componenet/Inputfield.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import "./style.css"
+
+interface Props{
+    todo:string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e:React.FormEvent) => void;
+}
+
+const Inputfield: React.FC<Props> = ({todo, setTodo, handleAdd}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+
+    return (
+    <form className='input' onSubmit={(e) => {
+        handleAdd(e)
+        inputRef.current?.blur()
+    }}>
+        <input 
+            type='input' 
+            ref={inputRef}
+            placeholder='Enter a task to do' 
+            className='input__box'
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+        />
+        <button className="input_submit" type='submit'>
+            Go
+        </button>
+    </form >
+  )
+}
+
+export default Inputfield
